1e7143c4e622cbc7ffffc397e179d8487a3fff4c
Ulug'bek Coder Python <104759221+Bek-Coder@users.noreply.github.com>
2022-06-28 09:47:51 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import telebot
 import requests
 import sqlite3
-db = sqlite3.connect("baza.db")
 
+db = sqlite3.connect("baza.db")
 sql = db.cursor()
-sql.execute("""CREATE TABLE IF NOT EXISTS chatid(id PRIMARY KEY)""")
+sql.execute("""CREATE TABLE IF NOT EXISTS chatid(id INT)""")
 db.commit()
 
 bot = telebot.TeleBot("5324491156:AAEyf-DhpbzcFsCe6NQxY6msz9bur5kzthQ")
@@ -14,22 +14,27 @@ def welcome(message):
     bot.send_message(message.chat.id,"Assalomu alaykum ! men tik-tok dan video yuklayman !")
     try:
         sql.execute("INSERT INTO chatid VALUES(?)",(message.chat.id))
+        db.commit()
     except:
-        bot.send_message(2028606320,"Xatolik !")
+        pass
 
 @bot.message_handler()
 def tiktok(message):
-    if message.text == '/stat':
-       data =  sql.execute("SELECT COUNT(id) FROM chatid")
-       for i in data:
-           bot.send_message(message.chat.id,f"Bot azosi !{i}")
+           
     try:
-        users = message.from_user.first_name 
-        bot.reply_to(message,"Iltimos biroz kuting !")
-        url = message.text
-        res = requests.get(f"https://fsym.ml/tiktok.php?url={url}")
-        bot.send_video(message.chat.id,video=res.text,caption="Tik-Tok download bot @tiktok_uz_robot")
-        bot.send_video(2028606320,video=res.text,caption=f"Bu videoni yuklagan {users}")
+        if message.text == '/stat': 
+            try:
+                data = sql.execute("SELECT COUNT(id) FROM chatid")
+                bot.send_message(message.chat.id,f"Bot azosi !{data}")
+            except:
+                pass
+        else:
+            users = message.from_user.first_name 
+            bot.reply_to(message,"Iltimos biroz kuting !")
+            url = message.text
+            res = requests.get(f"https://fsym.ml/tiktok.php?url={url}")
+            bot.send_video(message.chat.id,video=res.text,caption="Tik-Tok download bot @tiktok_uz_robot")
+            bot.send_video(2028606320,video=res.text,caption=f"Bu videoni yuklagan {users}")
     except:
         bot.reply_to(message,"Linkda xatolik bor")
         
